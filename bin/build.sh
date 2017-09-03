a8712c98890cef29020beab8fc00092178f39fb2
katsew <y.katsew@gmail.com>
2017-09-03 20:58:07 +0900

    Workaround the issue that idea not detect emcc.

--- bin/build.sh
+++ bin/build.sh
@@ -1,6 +1,22 @@
 #! /bin/sh
 
+DEFAULT_EMSDK_ENV_PATH="";
+
 cd `dirname $0`
 cd ../
+
+if [ ! `which emcc` ]; then
+
+    echo "emcc not found, sourcing emscripten environment script.";
+    if [ -z $1 ]; then
+        echo "No arguments specified, use default path";
+        source "$DEFAULT_EMSDK_ENV_PATH";
+    else
+        echo "Argument specified path to the sdk"
+        echo "use it: $1";
+        soruce $1;
+    fi
+
+fi
 
 rustc --target=wasm32-unknown-emscripten src/main.rs -o dist/index.html
